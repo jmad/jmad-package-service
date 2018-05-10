b3a7f5c10626dc0884f8e7c0cb1ce88db22cef9c
Kajetan Fuchsberger <kajetan.fuchsberger@cern.ch>
2018-05-10 06:16:45 +0200

    ongoing work

---
+++ src/java/org/jmad/modelpack/domain/ModelPackages.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2018 European Organisation for Nuclear Research (CERN), All Rights Reserved.
+ */
+
+package org.jmad.modelpack.domain;
+
+import static java.util.function.Function.identity;
+
+import java.util.Comparator;
+import java.util.function.Function;
+
+import org.jmad.modelpack.service.gitlab.domain.Release;
+import org.jmad.modelpack.service.gitlab.domain.Tag;
+
+/**
+ * Utility methods for jmad model packages
+ * 
+ * @author kfuchsbe
+ */
+public final class ModelPackages {
+
+    private ModelPackages() {
+        /* Only static methods */
+    }
+
+    public static Comparator<ModelPackageVariant> packageVariantComparator() {
+        return Comparator.<ModelPackageVariant, String> comparing(ti -> ti.modelPackage().name())
+                .thenComparing(ModelPackageVariant::variant, variantComparator());
+    }
+
+    public static Comparator<Variant> variantComparator() {
+        return Comparator.<Variant, Variant> comparing(identity(), (c1, c2) -> {
+            if ((c1 instanceof Release) && !(c2 instanceof Release)) {
+                return -1;
+            }
+            if ((c2 instanceof Release) && !(c1 instanceof Release)) {
+                return 1;
+            }
+
+            if ((c1 instanceof Tag) && !(c2 instanceof Tag)) {
+                return -1;
+            }
+            if ((c2 instanceof Tag) && !(c1 instanceof Tag)) {
+                return 1;
+            }
+            return 0;
+        }).thenComparing(Comparator.comparing(Variant::name).reversed());
+    }
+
+}
---
+++ src/test/org/jmad/modelpack/domain/ModelPackagesTest.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2018 European Organisation for Nuclear Research (CERN), All Rights Reserved.
+ */
+
+package org.jmad.modelpack.domain;
+
+import static org.jmad.modelpack.domain.ModelPackages.packageVariantComparator;
+import static org.jmad.modelpack.domain.ModelPackages.variantComparator;
+import static org.junit.Assert.*;
+
+import org.assertj.core.api.Assertions;
+import org.jmad.modelpack.service.gitlab.domain.Commit;
+import org.jmad.modelpack.service.gitlab.domain.Release;
+import org.jmad.modelpack.service.gitlab.domain.Tag;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import cern.accsoft.commons.util.MockitoUtil;
+
+public class ModelPackagesTest {
+
+    @Test
+    public void releaseIsFirst() {
+        
+        Commit anyCommit = Mockito.mock(Commit.class);
+        
+        Release release = new Release("", anyCommit);
+        Tag tag = new Tag("", anyCommit);
+        
+        Assertions.assertThat(variantComparator().compare(release, tag)).isEqualTo(-1);
+    }
+
+}
