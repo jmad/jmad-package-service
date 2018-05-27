56ff1e745e39c5088cf61099ccdc872c70069bae
Kajetan Fuchsberger <kajetan.fuchsberger@cern.ch>
2018-05-27 08:06:34 +0200

    making compile

--- src/java/org/jmad/modelpack/service/conf/JMadModelPackageServiceStandaloneConfiguration.java
+++ src/java/org/jmad/modelpack/service/conf/JMadModelPackageServiceStandaloneConfiguration.java
@@ -4,21 +4,9 @@
 
 package org.jmad.modelpack.service.conf;
 
-import cern.accsoft.steering.jmad.conf.JMadServiceConfiguration;
-import cern.accsoft.steering.jmad.util.TempFileUtil;
-import org.jmad.modelpack.service.JMadModelPackageService;
-import org.jmad.modelpack.service.ModelPackageFileCache;
-import org.jmad.modelpack.service.gitlab.conf.GitlabConnectorConfiguration;
-import org.jmad.modelpack.service.impl.ConcurrentModelPackageRepositoryManager;
-import org.jmad.modelpack.service.impl.ModelPackageFileCacheImpl;
-import org.jmad.modelpack.service.impl.MultiConnectorModelPackageService;
-import org.jmad.modelpack.service.internal.conf.InternalConnectorConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-import static org.jmad.modelpack.JMadModelRepositories.cernGitlabPro;
-import static org.jmad.modelpack.JMadModelRepositories.cernGitlabTesting;
-import static org.jmad.modelpack.JMadModelRepositories.internal;
+import cern.accsoft.steering.jmad.conf.JMadServiceConfiguration;
 
 /**
  * Ready to use configuration for the jmad-modelpack-service project. If you need more fine grained management of the
