1eba33c9e3c388156974f9f3e61097c35a4aed6a
Kajetan Fuchsberger <kajetan.fuchsberger@cern.ch>
2018-05-30 16:03:33 +0200

    merged

--- src/java/org/jmad/modelpack/domain/ModelPackage.java
+++ src/java/org/jmad/modelpack/domain/ModelPackage.java
@@ -43,60 +43,6 @@ public class ModelPackage {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((description == null) ? 0 : description.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((projectId == null) ? 0 : projectId.hashCode());
-        result = prime * result + ((repository == null) ? 0 : repository.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        ModelPackage other = (ModelPackage) obj;
-        if (description == null) {
-            if (other.description != null) {
-                return false;
-            }
-        } else if (!description.equals(other.description)) {
-            return false;
-        }
-        if (name == null) {
-            if (other.name != null) {
-                return false;
-            }
-        } else if (!name.equals(other.name)) {
-            return false;
-        }
-        if (projectId == null) {
-            if (other.projectId != null) {
-                return false;
-            }
-        } else if (!projectId.equals(other.projectId)) {
-            return false;
-        }
-        if (repository == null) {
-            if (other.repository != null) {
-                return false;
-            }
-        } else if (!repository.equals(other.repository)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
     public String toString() {
         return "ModelPackage [name=" + name + ", repository=" + repository + ", projectId=" + projectId
                 + ", description=" + description + "]";
@@ -104,13 +50,13 @@ public class ModelPackage {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         ModelPackage that = (ModelPackage) o;
-        return Objects.equals(name, that.name) &&
-                Objects.equals(repository, that.repository) &&
-                Objects.equals(projectId, that.projectId) &&
-                Objects.equals(description, that.description);
+        return Objects.equals(name, that.name) && Objects.equals(repository, that.repository)
+                && Objects.equals(projectId, that.projectId) && Objects.equals(description, that.description);
     }
 
     @Override
