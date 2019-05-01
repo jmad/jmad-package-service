9b347f8c15542bf4f79a382970ab0ad2c1706c6f
Michi <michi.hostettler@cern.ch>
2019-05-01 08:50:54 +0200

    filter invalid local modelpacks & tests

--- src/test/org/jmad/modelpack/connect/localfile/LocalFileModelPackageConnectorTest.java
+++ src/test/org/jmad/modelpack/connect/localfile/LocalFileModelPackageConnectorTest.java
@@ -15,6 +15,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.util.FileSystemUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -41,11 +42,17 @@ public class LocalFileModelPackageConnectorTest {
     @Before
     public void setUp() throws Exception {
         tmpRepoDir = Files.createTempDirectory("test-modelpack");
-        Files.createDirectory(modelpackFile("modelpack-1").toPath());
-        Files.createDirectory(modelpackFile("test-modelpack").toPath());
-        Files.createDirectory(modelpackFile("another-modelpack").toPath());
+        createModelpack("modelpack-1");
+        createModelpack("test-modelpack");
         repository = new JMadModelPackageRepository(tmpRepoDir.toAbsolutePath().toString(), "TEST", ConnectorIds.LOCAL_FILE_CONNECTOR_ID);
         when(jMadService.getModelDefinitionImporter()).thenReturn(importer);
+    }
+
+    private void createModelpack(String s) throws IOException {
+        Path dir = modelpackPath(s);
+        Files.createDirectory(dir);
+        File xmlFile = new File(dir.toAbsolutePath() + File.separator + "model.jmd.xml");
+        xmlFile.createNewFile();
     }
 
     @After
@@ -53,9 +60,9 @@ public class LocalFileModelPackageConnectorTest {
         FileSystemUtils.deleteRecursively(tmpRepoDir);
     }
 
-    private File modelpackFile(String name) {
+    private Path modelpackPath(String name) {
         String tmpPrefix = tmpRepoDir.toAbsolutePath().toString() + File.separator;
-        return new File(tmpPrefix + name);
+        return new File(tmpPrefix + name).toPath();
     }
 
     @Test
@@ -74,7 +81,7 @@ public class LocalFileModelPackageConnectorTest {
         ModelPackage modelPackage = new ModelPackage("modelpack-1", repository, "modelpack-1", "Test");
         ModelPackageVariant variant = new ModelPackageVariant(modelPackage, Variant.release("LOCAL", new Commit("LOCAL", "LOCAL")));
         connector.modelDefinitionsFor(variant).blockFirst();
-        verify(importer, times(1)).importModelDefinitions(modelpackFile("modelpack-1"));
+        verify(importer, times(1)).importModelDefinitions(modelpackPath("modelpack-1").toFile());
     }
 
     @Test
@@ -95,11 +102,22 @@ public class LocalFileModelPackageConnectorTest {
     }
 
     @Test
-    public void availablePackages_forSupportedModelRepo_shouldListModelPacks() {
+    public void availablePackages_forSupportedModelRepo_shouldListModelPacks() throws Exception {
         List<ModelPackageVariant> allModelPacks = connector.availablePackages(repository).collectList().block();
         assertThat(allModelPacks.stream().map(ModelPackageVariant::modelPackage).map(ModelPackage::id))
-                .containsExactlyInAnyOrder("modelpack-1", "test-modelpack", "another-modelpack");
+                .containsExactlyInAnyOrder("modelpack-1", "test-modelpack");
         assertThat(allModelPacks.stream().map(ModelPackageVariant::modelPackage).map(ModelPackage::name))
-                .containsExactlyInAnyOrder("LOCAL-modelpack-1", "LOCAL-test-modelpack", "LOCAL-another-modelpack");
+                .containsExactlyInAnyOrder("LOCAL-modelpack-1", "LOCAL-test-modelpack");
+    }
+
+    @Test
+    public void availablePackages_forSupportedRepoWithSomeInvalidSubDirs_shouldFilterOutFakeDirs() throws Exception {
+        Files.createDirectory(modelpackPath("NOT-A-modelpack"));
+        Files.createDirectory(modelpackPath("YET-another-FAKE-modelpack"));
+        List<ModelPackageVariant> allModelPacks = connector.availablePackages(repository).collectList().block();
+        assertThat(allModelPacks.stream().map(ModelPackageVariant::modelPackage).map(ModelPackage::id))
+                .doesNotContain("NOT-A-modelpack", "YET-another-FAKE-modelpack");
+        assertThat(allModelPacks.stream().map(ModelPackageVariant::modelPackage).map(ModelPackage::name))
+                .doesNotContain("LOCAL-NOT-A-modelpack", "LOCAL-YET-another-FAKE-modelpack");
     }
 }
